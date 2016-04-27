7e913336b911b7d5c78bd38c0d1ebff3c3c75755
zietz <michael.zietz@gmail.com>
2016-04-26 23:21:35 -0400

    Changes

--- src/layer/bash-gen-bilayer.sh
+++ src/layer/bash-gen-bilayer.sh
@@ -14,16 +14,21 @@ set -e
 ### top-martini-v2.1.itp
 
 source /usr/local/gromacs/bin/GMXRC
+
 genbox -ci str-dxpc-single.gro -box 6.31915 6.46099 10.05482 -nmol 128 -try 1000 -o gmx-vac-lipids.gro
 python py-top-builder.py gmx-vac-lipids.gro gmx-vac.top
+
 grompp -f em.mdp -c gmx-vac-lipids.gro -p gmx-vac.top -maxwarn 10 -o em-vac.tpr
 mdrun -deffnm em-vac -v
+
 genbox -cp em-vac.gro -cs str-water.gro -vdwd 0.21 -maxsol 2000 -box 6.5 6.5 12.5 -o gmx-lipids.gro
-python py-top-builder.py gmx-lipids.gro gmx-lipids.top
-grompp -f em.mdp -c gmx-lipids.gro -p gmx-lipids.top -maxwarn 10 -o em-lipids.tpr
+python py-top-builder.py gmx-lipids.gro gmx-bilayer.top
+
+grompp -f em.mdp -c gmx-lipids.gro -p gmx-bilayer.top -maxwarn 10 -o em-lipids.tpr
 mdrun -deffnm em-lipids -v
-grompp -f md-bilayer.mdp -c em-lipids.gro -p gmx-lipids.top -maxwarn 10 -o md-lipids.tpr
+
+grompp -f md-bilayer.mdp -c em-lipids.gro -p gmx-bilayer.top -maxwarn 10 -o md-lipids.tpr
 mdrun -deffnm md-lipids -v
+
 echo "0" | trjconv -f md-lipids.xtc -s md-lipids.tpr -pbc mol -o gmx-bilayer.xtc
 echo "0" | trjconv -f gmx-bilayer.xtc -s md-lipids.tpr -dump 30000 -vel -o gmx-bilayer.gro
-cp gmx-lipids.top gmx-bilayer.top
